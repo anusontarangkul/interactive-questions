93ac5348737a74381053c03265af5710565dea58
David Anusontarangkul <anusontarangkul.d@gmail.com>
2021-11-09 22:30:13 -0800

    functionality of entering name

--- src/Components/Dialogue/Dialogue.jsx
+++ src/Components/Dialogue/Dialogue.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Container from '@mui/material/Container';
 import BotName from './BotName/BotName';
 import UserName from './UserName/UserName';
 
 const Dialogue = () => {
+  const [name, setName] = useState('');
   return (
     <Container>
       <BotName />
-      <UserName />
+      <UserName name={name} setName={setName} />
     </Container>
   );
 };
--- src/Components/Dialogue/UserName/UserName.jsx
+++ src/Components/Dialogue/UserName/UserName.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './UserName.css';
 
@@ -9,11 +9,57 @@ import TextField from '@mui/material/TextField';
 
 import DoubleArrowOutlinedIcon from '@mui/icons-material/DoubleArrowOutlined';
 
-const UserName = () => {
+const UserName = ({ name, setName }) => {
+  const [completedName, setCompletedName] = useState(false);
+  const [initial, setInitial] = useState('');
+
+  const submitNameHandler = (e) => {
+    // console.log(initial);
+    setInitial(name[0]);
+    setCompletedName(true);
+  };
+
+  const nameChangeHandler = (e) => {
+    let updatedName = e.target.value;
+    setName(updatedName);
+  };
+  if (completedName) {
+    return (
+      <Box
+        sx={{
+          marginTop: '35px',
+          marginLeft: 'auto',
+          display: 'flex',
+          border: '2px black solid',
+          maxWidth: '500px',
+          borderRadius: '10px',
+          alignItems: 'center',
+          padding: '10px 10px',
+          backgroundColor: '#4180ec',
+          color: 'white',
+        }}
+      >
+        <Typography sx={{ fontSize: '22px', marginLeft: '5px' }}>
+          Hi Tapply Bot! My name is <strong>{name}</strong>.
+        </Typography>
+        <Avatar
+          sx={{
+            height: '50px',
+            width: '50px',
+            // marginLeft: 'auto',
+            // marginRight: '5px',
+            margin: '6.5px 5px 6.5px auto',
+          }}
+        >
+          {initial}
+        </Avatar>
+      </Box>
+    );
+  }
   return (
     <Box
       sx={{
-        marginTop: '40px',
+        marginTop: '35px',
         marginLeft: 'auto',
         display: 'flex',
         border: '2px black solid',
@@ -26,9 +72,10 @@ const UserName = () => {
       }}
     >
       <TextField
+        onChange={nameChangeHandler}
         id='outlined-basic'
         label='Enter Name Here..'
-        variant='outlined'
+        // variant='outlined'
         required
         autoComplete='off'
         className='text-root'
@@ -39,20 +86,18 @@ const UserName = () => {
           className: 'textfield__border',
         }}
         sx={{
-          // padding: 'px',
-
           margin: '5px 15px 5px 5px',
           width: '400px',
-
-          '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline': {
-            borderColor: 'white',
-            '& label.Mui-focused': {
-              borderColor: 'white',
-            },
+          '& .MuiOutlinedInput-root .MuiOutlinedInput-notchedOutline ': {
+            borderColor: 'white !important',
+          },
+          '&.Mui-focused .MuiOutlinedInput-notchedOutline': {
+            borderColor: 'white !important',
           },
         }}
       />
       <Avatar
+        onClick={submitNameHandler}
         sx={{
           cursor: 'pointer',
           marginLeft: 'auto',
